d76c3496a0299313ff33cf5c41071ddbda13f055
dialahamdan <dialahamdan9@gmail.com>
2024-02-13 23:59:30 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -2,15 +2,17 @@ let button= document.querySelector(".btn")
 let sideBar = document.querySelector(".sideBar")
 let btnClose = document.querySelector(".btn-close")
 let offcanvas = document.querySelector(".offcanvas-start")
-
+let body = document.querySelector("body")
 button.onclick=function(){
     sideBar.classList.remove("sideBar");
     offcanvas.classList.add("show")
+    body.classList.add("overlay")
 };
  
 
 btnClose.onclick=function(){
     sideBar.classList.add("sideBar");
+    body.classList.remove("overlay")
     
 };
 
